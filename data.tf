c7e7171b96efb69751e7501ab59bb4e75f955efc
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2023-05-09 14:48:51 +0200

    Update data.tf to address deprecation warning

--- data.tf
+++ data.tf
@@ -10,8 +10,10 @@ data "aws_identitystore_group" "this" {
   for_each          = toset(local.sso_groups)
   identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
 
-  filter {
-    attribute_path  = "DisplayName"
-    attribute_value = each.value
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = each.value
+    }
   }
 }
